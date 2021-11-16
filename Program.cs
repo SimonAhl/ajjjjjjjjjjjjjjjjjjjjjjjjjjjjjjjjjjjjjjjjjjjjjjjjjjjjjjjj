5d30ce52b466a5dd256b4d8a649c108d519b025b
naal01002 <89377694+naal01002@users.noreply.github.com>
2021-11-16 13:53:21 +0100

    nej

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace jag_är_inte_du
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float öjjj = 2;
+            int ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo = 10;
+
+            Console.WriteLine("hhhhhhh " + POOOWERRR( öjjj, ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo));
+
+
+        }
+        static double POOOWERRR( float bot, int top){
+            double aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = bot;
+            double ajjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj = top;
+
+            return(Math.Pow(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, ajjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj));
+        }
+    }
+}
